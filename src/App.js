43a08057b224d6bfe5129fe1f0b6934127a2e638
Akshay Bodla <akshaybodla21@gmail.com>
2022-11-13 15:17:21 -0600

    Template for task manager

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TaskContainer from './components/TaskContainer';
+
+function App() {
+    return (
+        <div className='flex justify-center my-10'>
+            <TaskContainer />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/TaskContainer.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react';
+
+const TaskContainer = () => {
+    return (
+        <div className='flex flex-col gap-8 border-2 border-slate-600 w-[75vh] rounded-lg p-8'>
+            Task Manager
+        </div>
+    );
+};
+
+export default TaskContainer;
